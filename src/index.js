7ff17855a0262a697086568826b68ef912d25835
JagTheFriend <jagthefriend12@gmail.com>
2021-10-11 17:57:38 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -13,14 +13,14 @@ const createWindow = () => {
         height: 600,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        autoHideMenuBar: true
     });
-
+    mainWindow.on("ready-to-show", mainWindow.show);
     // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
